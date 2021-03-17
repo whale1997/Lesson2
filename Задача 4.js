871cc084ed41dc349d7cceffe0f20ffde6ee9028
whale1997 <80566174+whale1997@users.noreply.github.com>
2021-03-17 17:00:35 +0700

    Add files via upload

---
+++ Задача 4.js
@@ -0,0 +1,50 @@
+       <!-- Задача 4. -->
+        a = +prompt ('Задача 4. Введите число от 1 до 15');
+		switch (a) {
+		case 1:
+			alert ( 'Ваше число 1');
+			break;
+		case 2:
+			alert ( 'Ваше число 2');
+			break;
+		case 3:
+			alert ( 'Ваше число 3');
+			break;
+		case 4:
+			alert ( 'Ваше число 4');
+			break;
+		case 5:
+			alert ( 'Ваше число 5');
+			break;
+		case 6:
+			alert ( 'Ваше число 6');
+			break;
+		case 7:
+			alert ( 'Ваше число 7');
+			break;
+		case 8:
+			alert ( 'Ваше число 8');
+			break;
+		case 9:
+			alert ( 'Ваше число 9');
+			break;
+		case 10:
+			alert ( 'Ваше число 10');
+			break;
+		case 11:
+			alert ( 'Ваше число 11');
+			break;
+		case 12:
+			alert ( 'Ваше число 12');
+			break;
+		case 13:
+			alert ( 'Ваше число 13');
+			break;
+		case 14:
+			alert ( 'Ваше число 14');
+			break;
+		case 15:
+			alert ( 'Ваше число 15');
+			break;	
+		}	
+
